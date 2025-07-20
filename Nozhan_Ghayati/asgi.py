f1338a0d4a4cca6b6fcb014200568ce8749280e0
Mrghayati <ghayati2006@gmail.com>
2025-07-20 13:36:41 +0330

    Initial commit: Enhanced portfolio website

---
+++ Nozhan_Ghayati/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Nozhan_Ghayati project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Nozhan_Ghayati.settings')
+
+application = get_asgi_application()
